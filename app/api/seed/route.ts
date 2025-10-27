291eb6b0e2c65e81ca28fc436638a4f0f9ae2b90
Yash-0006 <yaswanthgodavarthi0006@gmail.com>
2025-10-27 16:12:18 +0530

    updated route.ts

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,6 +1,7 @@
 import { neon } from "@neondatabase/serverless"
 import { drizzle } from "drizzle-orm/neon-http"
 import { users, categories, posts, postCategories } from "@/lib/schema"
+import "dotenv/config"
 
 const sql = neon(process.env.NEON_NEON_DATABASE_URL!)
 const db = drizzle(sql, { schema: { users, categories, posts, postCategories } })
